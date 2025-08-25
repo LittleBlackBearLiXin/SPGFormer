145bb29889bd865f6a930095aed71c7cd4fed39d
Little black bear <lixin17808627537@163.com>
2025-08-26 07:37:02 +0800

    Update our_SLIC.py

--- SPGFormer/SPGformer_MODEL/SLIC/our_SLIC.py
+++ SPGFormer/SPGformer_MODEL/SLIC/our_SLIC.py
@@ -23,9 +23,6 @@ def fix_seed(seed):
 
 fix_seed(42)
 
-
-
-
 def graph(adj, name,dir):
     plt.figure(figsize=(8, 6))
     sns.heatmap(adj.numpy(), cmap='Blues', annot=False, cbar=True)
@@ -43,15 +40,12 @@ def graph(adj, name,dir):
     print(f"Figure saved to {save_path}")  #
 
 def SegmentsLabelProcess(labels):
-
     labels = np.array(labels, np.int64)
     H, W = labels.shape
     ls = list(set(np.reshape(labels, [-1]).tolist()))
-
     dic = {}
     for i in range(len(ls)):
         dic[ls[i]] = i
-
     new_labels = labels
     for i in range(H):
         for j in range(W):
@@ -73,8 +67,7 @@ class SLICcccc(object):
         min_max = preprocessing.StandardScaler()
         data = min_max.fit_transform(data)
         self.data = np.reshape(data, [height, width, bands])
-
-
+                     
     def get_Q_and_S_and_Segments(self):
         img = self.data
         (h, w, d) = img.shape
@@ -85,50 +78,23 @@ class SLICcccc(object):
         if segments.max() + 1 != len(list(set(np.reshape(segments, [-1]).tolist()))):
             segments = SegmentsLabelProcess(segments)
         self.segments = segments
-
-
         superpixel_count = segments.max() + 1  # =segments.max() + 1
         self.superpixel_count = superpixel_count
         print("superpixel_count", superpixel_count)
 
         segments_1 = np.reshape(segments, [-1])
-        S = np.zeros([superpixel_count, d], dtype=np.float32)  # 超像素特征
-
-        Q = np.zeros([w * h, superpixel_count], dtype=np.float32)  # 像素与超像素联系矩阵
-        x = np.reshape(img, [-1, d])  # Flatten(x)
-
-        x_center = np.zeros([superpixel_count], dtype=np.float32)
-        y_center = np.zeros([superpixel_count], dtype=np.float32)
-
+        Q = np.zeros([w * h, superpixel_count], dtype=np.float32) 
         for i in range(superpixel_count):
             idx = np.where(segments_1 == i)[0]
-            count = len(idx)
-            pixels = x[idx]
-            superpixel = np.sum(pixels, 0) / count
-            S[i] = superpixel
             Q[idx, i] = 1
-
-            seg_idx = np.where(segments == i)
-            x_center[i] = np.mean(seg_idx[0])
-            y_center[i] = np.mean(seg_idx[1])
-
-        self.S = S
-        self.Q = Q
-
-
-
         return Q
-
-
+        
     def get_A(self):
-
         A = np.zeros([self.superpixel_count, self.superpixel_count], dtype=np.float32)
         centr_pos = np.zeros([self.superpixel_count, 2], dtype=np.float32)  # 每个超像素的质心坐标 (x, y)
         (h, w) = self.segments.shape
         #print(self.segments)
         #print(self.segments.shape)
-
-
         for i in range(h - 1):
             for j in range(w - 1):
                 sub = self.segments[i:i + 2, j:j + 2]
@@ -141,33 +107,23 @@ class SLICcccc(object):
                     if A[idx1, idx2] != 0:
                          continue
                     A[idx1, idx2] = A[idx2, idx1] = 1
-
         return A
-
-
+        
 class Segmenttttt(object):
     def __init__(self, data, n_component):
         self.data = data
         self.n_component = n_component
         self.height, self.width, self.bands = data.shape
-
-
+        
     def SLIC_Process(self, img, scale=25):
         n_segments_init = self.height * self.width / scale
         print("n_segments_init", n_segments_init)
-
         myslic = SLICcccc(img, n_segments=n_segments_init, sigma=1, min_size_factor=0.1,
                       max_size_factor=10)
-
         Q= myslic.get_Q_and_S_and_Segments()
         A = myslic.get_A()
         return Q,  A
-
-
-
-
-
-
+        
 def noramlize(A,random_wolk=False):
     A = A + torch.eye(A.shape[0], A.shape[0]).to(device)
     if random_wolk:
@@ -189,7 +145,6 @@ def spareaA(adjacency):
     adjacency = torch.sparse_coo_tensor(indices, values, size, dtype=torch.float32, device=device)
     return adjacency
 
-
 def Ahop(adjacency, K):
     result = adjacency.clone()
     for i in range(K - 1):
@@ -204,3 +159,4 @@ def getimportA(adjacency, k=2, FLAG=0):
         imadjaceny = imadjaceny
     imadjaceny = torch.where(imadjaceny > k-1, 1, 0).float()
     return imadjaceny
+
